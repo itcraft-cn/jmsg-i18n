8c12689511651d8a09ce5342686f2dd21059db05
Helly Guo <hellyguo@foxmail.com>
2026-04-20 18:18:33 +0800

    feat: add hasLocale/getDefaultLocale to MsgTemplate interface

--- src/main/java/cn/itcraft/jmsg/core/MsgTemplate.java
+++ src/main/java/cn/itcraft/jmsg/core/MsgTemplate.java
@@ -2,16 +2,23 @@ package cn.itcraft.jmsg.core;
 
 import cn.itcraft.dyenums.core.DyEnum;
 import java.util.Locale;
+import java.util.Set;
 
 public interface MsgTemplate extends DyEnum {
     
     Locale getLocale();
+    
+    Locale getDefaultLocale();
     
     MsgTemplate.Style getStyle();
     
     String render(Locale locale, Object... args);
     
     String render(Object... args);
+    
+    Set<String> getSupportedLocales();
+    
+    boolean hasLocale(Locale locale);
     
     enum Style {
         SIMPLE,
